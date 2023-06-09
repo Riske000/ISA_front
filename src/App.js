8b44f0c6d6f85a25cdffc98e23c74eb6bebf32c2
Stasa <stasapetrovicns@gmail.com>
2023-06-09 11:27:22 +0200

    AuthGuard updated

--- src/App.js
+++ src/App.js
@@ -21,32 +21,25 @@ import ProtectedRoute from "./auth/AuthGuard";
 function App() {
   const navigate = useNavigate();
   const [loggedUser, setLoggedUser] = useState(null);
+  console.log(localStorage.getItem("currentUser"))
 
   return (
     <Routes>
       <Route index path="/" element={<AllCenters />} />
       <Route path="/login" element={<LoginForm />} />
       <Route path="/registration" element={<RegistrationForm />} />
-
-      <Route
-        element={
-          <ProtectedRoute
-            isAllowed={loggedUser !== null}
-            redirectPath="/"
-          >
-            <Route path="/homePageRegisteredUser" element={<HomePageRegisteredUser />} />
-            <Route path="/homePageAdmin" element={<HomePageAdmin />} />
-            <Route path="/calendar" element={<MyCalendar />} />
-            <Route path="/addTerm" element={<TermForm />} />
-            <Route path="/homePageAdminCenter" element={<HomePageAdminCenter />} />
-            <Route path="/adminsCenter" element={<AdminsCenterDetails />} />
-            <Route path="/myProfile" element={<MyProfile />} />
-            <Route path="/newTerm" element={<NewTerm />} />
-            <Route path="/questionnaire" element={<Questionnaire />} />
-            <Route path="/changePassword" element={<ChangePassword />} />
-          </ProtectedRoute>
-        }
-      />
+      <Route element = { <ProtectedRoute redirectPath="/" isAllowed={localStorage.getItem("currentUser")}/> }>
+        <Route path="/homePageRegisteredUser" element={<HomePageRegisteredUser />} />
+        <Route path="/homePageAdmin" element={<HomePageAdmin />} />
+        <Route path="/calendar" element={<MyCalendar />} />
+        <Route path="/addTerm" element={<TermForm />} />
+        <Route path="/homePageAdminCenter" element={<HomePageAdminCenter />} />
+        <Route path="/adminsCenter" element={<AdminsCenterDetails />} />
+        <Route path="/myProfile" element={<MyProfile />} />
+        <Route path="/newTerm" element={<NewTerm />} />
+        <Route path="/questionnaire" element={<Questionnaire />} />
+        <Route path="/changePassword" element={<ChangePassword />} />
+      </Route>
     </Routes>
   );
 }
