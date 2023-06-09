304075b1a370504afdb36c8df31c189ded841c83
Stasa <stasapetrovicns@gmail.com>
2023-06-09 09:11:12 +0200

    AuthGuard added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import LoginForm from "./components/LoginForm";
 import RegistrationForm from "./components/RegistrationForm";
 import "./css/logOutButton.css";
 import "./css/form.css";
-import {
-  Route,
-  Routes,
-  useNavigate
-} from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import HomePageRegisteredUser from "./components/HomePageRegisteredUser";
 import HomePageAdmin from "./components/HomePageAdmin";
 import MyCalendar from "./components/Calendar";
@@ -21,48 +16,39 @@ import NewTerm from "./components/NewTerm";
 import Questionnaire from "./components/Questionnaire";
 import AllCenters from "./components/AllCenters";
 import ChangePassword from "./components/ChangePassword";
-
-
-
+import ProtectedRoute from "./auth/AuthGuard";
 
 function App() {
   const navigate = useNavigate();
-  const [loggedUser, setLogedUserType] = useState(null);
-  
+  const [loggedUser, setLoggedUser] = useState(null);
 
   return (
-      <Routes >
-        <Route index path="/" element={<AllCenters />} />
-        <Route index path="/login" element={<LoginForm />} />
-        <Route path="/registration" element={<RegistrationForm />} />
-        <Route path="/homePageRegisteredUser" element={<HomePageRegisteredUser />} />
-        <Route path="/homePageAdmin" element={<HomePageAdmin />} />
-        <Route path="/calendar" element={<MyCalendar />} />
-        <Route path="/addTerm" element={<TermForm />} />
-        <Route path="/homePageAdminCenter" element={<HomePageAdminCenter />} />
-        <Route path="/adminsCenter" element={<AdminsCenterDetails />} />
-        <Route path="/myProfile" element={<MyProfile />} />
-        <Route path="/newTerm" element={<NewTerm />} />
-        <Route path="/questionnaire" element={<Questionnaire />} />
-        <Route path="/changePassword" element={<ChangePassword />} />
-      </Routes>
+    <Routes>
+      <Route index path="/" element={<AllCenters />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/registration" element={<RegistrationForm />} />
+
+      <Route
+        element={
+          <ProtectedRoute
+            isAllowed={loggedUser !== null}
+            redirectPath="/"
+          >
+            <Route path="/homePageRegisteredUser" element={<HomePageRegisteredUser />} />
+            <Route path="/homePageAdmin" element={<HomePageAdmin />} />
+            <Route path="/calendar" element={<MyCalendar />} />
+            <Route path="/addTerm" element={<TermForm />} />
+            <Route path="/homePageAdminCenter" element={<HomePageAdminCenter />} />
+            <Route path="/adminsCenter" element={<AdminsCenterDetails />} />
+            <Route path="/myProfile" element={<MyProfile />} />
+            <Route path="/newTerm" element={<NewTerm />} />
+            <Route path="/questionnaire" element={<Questionnaire />} />
+            <Route path="/changePassword" element={<ChangePassword />} />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
 
 export default App;
-
-/*<div>
-    <div className="App">
-      {appState === "login" && (
-        <LoginForm appState={setAppState} userType={setLogedUserType} />
-      )}
-      {appState === "register" && <RegistrationForm appState={setAppState} />}
-    </div>
-    {appState !== "login" && appState !== "register" && (
-      <div className="div-right_bottom ">
-        <button className="button-right_bottom" onClick={onLogOutClick}>
-          Log out
-        </button>
-      </div>
-    )}
-  </div>*/
---
+++ src/auth/AuthGuard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router";
+
+
+const ProtectedRoute = ({ isAllowed, redirectPath = "/", children }) => {
+    if(!isAllowed){
+        return <Navigate to={redirectPath} replace />;
+    }
+
+    return children ? children : <Outlet />;
+};
+
+export default ProtectedRoute;
